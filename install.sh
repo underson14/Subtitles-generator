8882dd9fe89afe62d89e963f8c1eaa6a0f254759
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2021-02-05 00:36:22 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 python -m pip install --upgrade pip
 pip3 uninstall librosa -y
 pip3 install librosa==0.6.2
+pip3 uninstall numba
+pip install numba==0.48
 pip3 install scipy
 pip3 install SpeechRecognition
 pip3 install youtube-dl
