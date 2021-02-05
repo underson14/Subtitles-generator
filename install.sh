e2a24ae5073f1e6c52dd5c6b167ade68371d1754
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2021-02-05 00:51:21 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 export PATH=/usr/local/bin:$PATH\
 python -m pip install --upgrade pip
 pip3 uninstall librosa -y
-pip3 install librosa==0.6.2
+pip3 install librosa==0.6.2 --use-feature=2020-resolver
 pip3 uninstall numba -y
-pip install numba==0.48
-pip3 install scipy
-pip3 install SpeechRecognition
-pip3 install youtube-dl
+pip install numba==0.48 --use-feature=2020-resolver
+pip3 install scipy --use-feature=2020-resolver
+pip3 install SpeechRecognition --use-feature=2020-resolver
+pip3 install youtube-dl --use-feature=2020-resolver
