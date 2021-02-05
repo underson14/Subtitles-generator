4a94bbe579d60e0fd10460abcd4acee5f0056999
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2021-02-05 00:37:32 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 python -m pip install --upgrade pip
 pip3 uninstall librosa -y
 pip3 install librosa==0.6.2
-pip3 uninstall numba
+pip3 uninstall numba -y
 pip install numba==0.48
 pip3 install scipy
 pip3 install SpeechRecognition
