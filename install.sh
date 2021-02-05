efe59481484dae61e71884e4fd3a7f9e23421276
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2021-02-05 01:08:57 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 python -m pip install --upgrade pip
-pip install umap-learn
-pip install pynndescent
-pip install numpy==1.15.1
 pip uninstall librosa -y
 pip install librosa==0.6.2
 pip uninstall numba -y
-pip install numba==0.48
+pip install numba==0.49
 pip uninstall scipy -y
 pip install scipy==1.0.0
 pip install SpeechRecognition==3.8.1
