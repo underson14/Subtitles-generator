a6845a07b8522fe4f11365e57849e0e32e942fe8
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2021-02-05 00:49:02 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+export PATH=/usr/local/bin:$PATH\
 python -m pip install --upgrade pip
 pip3 uninstall librosa -y
 pip3 install librosa==0.6.2
